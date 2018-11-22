068fcc6f688806bc1bb4f5cb46d9c96e149fc6b0
andruchkov <andruchkov@yandex-team.ru>
2018-11-22 09:52:46 +0300

    доделал инпут

---
+++ app/src/components/Icon/bolt.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import img from  './i/basic_bolt.svg';
+
+export default () => {
+	return <span className="icon" style={{backgroundImage: `url(${img})`}}/>;
+}
---
+++ app/src/components/Icon/removeCircle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import img from  './i/arrows_circle_remove.svg';
+
+export default () => {
+	return <span className="icon" style={{backgroundImage: `url(${img})`}}/>;
+}
