174c0acee605b9ce03d01dd498f8da09c5b71855
vit134 <vit134@mail.ru>
2018-12-17 11:04:14 +0300

    change logo

--- app/src/components/Logo/Logo.js
+++ app/src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Logo.css';
+import logo from './list.svg';
 
 export default () => {
     return (
         <div className="logo">
-            <div className="logo__image" />
+            <img src={logo} className="logo__image" alt="logo"/>
             <div className="logo__text">Wishlist</div>
         </div>
     );
--- app/src/containers/App/App.js
+++ app/src/containers/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
+import { Layout } from 'antd';
+
 import User from '../../components/User/User';
 import Page from '../../components/Page/Page';
 import Overlay from '../../components/Overlay/Overlay';
@@ -10,6 +13,8 @@ import * as userActions from '../../actions/UserActions';
 import * as overlayActions from '../../actions/OverlayActions';
 
 import './App.css';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
     componentDidMount() {
@@ -28,24 +33,22 @@ class App extends Component {
     }
 
     render() {
-        const { user, page, overlay } = this.props;
-        const { checkLogin, registration, logout } = this.props.userActions;
-        const { toggleOverlay } = this.props.overlayActions;
-
-        return ([
-            <header>
-                <div className="container">
-                    <Logo />
-                    <User {...this.props} />
-                </div>
-            </header>,
-            <main>
-                <div className="container">
-                    <Page  {...this.props}/>
-                </div>
-            </main>,
-            <Overlay { ...this.props}/>
-        ]);
+        return (
+            <Layout>
+                <Header>
+                    <div className="container">
+                        <Logo />
+                        <User {...this.props} />
+                    </div>
+                </Header>,
+                <Content>
+                    <div className="container">
+                        <Page  {...this.props}/>
+                    </div>
+                </Content>,
+                <Overlay { ...this.props}/>
+            </Layout>
+        );
     }
 }
 
