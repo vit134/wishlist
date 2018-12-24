ba887c571140398d6a7743d0e57a8829fdd984be
vit134 <vit134@mail.ru>
2018-12-24 16:48:28 +0300

    change login form

--- app/src/components/Forms/RegForm.js
+++ app/src/components/Forms/RegForm.js
@@ -1,7 +1,74 @@
 import React from 'react'
 import './Forms.css';
 
-import Preloader from '../Preloader/Preloader';
+
+import {
+	Form, Icon, Input, Button, Checkbox, Card
+} from 'antd';
+
+class NormalLoginForm extends React.Component {
+	handleSubmit = (e) => {
+
+		e.preventDefault();
+		const { login } = this.props.userActions;
+		const { toggleOverlay } = this.props.overlayActions;
+
+		this.props.form.validateFields((err, values) => {
+			if (!err) {
+				console.log('Received values of form: ', values);
+				login(values)
+					.then((resp) => {
+						return this.props.pageActions.getWhishes(resp.user._id);
+					})
+					.then(() => {
+						toggleOverlay();
+					})
+					.catch(e => console.log(e));
+			}
+		});
+	}
+
+	render() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<Card>
+				<Form onSubmit={this.handleSubmit} className="login-form">
+					<Form.Item>
+						{getFieldDecorator('username', {
+							rules: [{ required: true, message: 'Please input your username!' }],
+						})(
+							<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('password', {
+							rules: [{ required: true, message: 'Please input your Password!' }],
+						})(
+							<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('remember', {
+							valuePropName: 'checked',
+							initialValue: true,
+						})(
+							<Checkbox>Remember me</Checkbox>
+						)}
+						<a className="login-form-forgot" href="http://">Forgot password</a>
+						<Button type="primary" htmlType="submit" className="login-form-button">
+							Log in
+						</Button>
+						Or <a href="http://">register now!</a>
+					</Form.Item>
+				</Form>
+			</Card>
+		);
+	}
+}
+
+export default Form.create()(NormalLoginForm);
+
+/* import Preloader from '../Preloader/Preloader';
 
 export default class RegForm extends React.Component {
 	constructor(props) {
@@ -103,4 +170,4 @@ export default class RegForm extends React.Component {
 			</div>
 		);
 	}
-}
+} */
